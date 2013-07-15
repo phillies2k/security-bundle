62581f627a09823458746d6ee72cb8a324afb87f
phillies2k <mostgreedy@gmail.com>
2013-07-15 20:15:05 +0200

    initial commit

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,9 +13,8 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ * Class Configuration
+ * @package P2\Bundle\SecurityBundle\DependencyInjection
  */
 class Configuration implements ConfigurationInterface
 {
@@ -26,10 +25,6 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('p2_security');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         $rootNode
             ->children()
